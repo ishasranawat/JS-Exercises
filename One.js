c25e0dc0ad9497723e8c14554c81773899033217
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-11-13 11:04:15 +0530

    Update One.js

--- One.js
+++ One.js
@@ -1,4 +1,4 @@
-// switch variables
+//1. switch variables
 let a= 3
 let b=5
 
@@ -8,3 +8,15 @@ b=c
 
 console.log(a)
 console.log(b)
+
+
+// 2. take input and only capitalise the first letter
+
+let input=prompt("What is your name?")
+
+snip=input.slice(0,1)
+rest=input.slice(1)
+
+let name= snip.toUpperCase()
+
+console.log("Hello" + " " + name + rest)
