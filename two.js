a113d9c8f2e93861d92ac5697f841cd34869ffb2
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-11-17 16:00:04 +0530

    Update two.js

--- two.js
+++ two.js
@@ -15,3 +15,17 @@ else {
 }
 
 bmiCalculator(49,1.5)
+
+
+// Leap year 
+function isLeap(year) {
+    
+if ((year%4===0 && year%100!==0) || (year%400===0)) {
+    return "Leap year."; 
+}
+else {
+    return "Not leap year.";
+}
+
+isLeap(2024)
+
