c2bc696970dca773791ea3ea56b432e3696bc41c
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-01-19 11:05:04 +0530

    Update week 2.js

--- cohort/week 2.js
+++ cohort/week 2.js
@@ -1,1 +1,18 @@
+// sum of 1 to n
+// 1.
+function sum(n){
+  let ans=0;
+  for ( let i=1; i<=n; i++) {
+    ans= ans+ i
+  }
+  return ans;
+}
+const ans=sum(10);
+console.log(ans)
 
+// 2.
+function sum(n){
+  return (n*(n+1)/2);
+  }
+
+console.log(sum(10));
