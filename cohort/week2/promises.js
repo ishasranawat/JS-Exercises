5bfad22ae3dd91b373152cb4b3f0e1017334f72c
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-02-01 12:38:04 +0530

    Create promises.js

---
+++ cohort/week2/promises.js
@@ -0,0 +1,12 @@
+// Date class
+const date= new Date();
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDay());
+
+
+// Map class
+const map= new Map();
+map.set('name', 'Isha');
+map.set('age', '22');
+console.log(map.get('age'));
