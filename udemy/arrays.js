885cc2a20f8ee9db2d4c92fa8f942c0443093dde
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-02-03 14:33:46 +0530

    Create arrays.js

---
+++ udemy/arrays.js
@@ -0,0 +1,41 @@
+* 
+1. Declare an array named `teaFlavors` that contains the strings `"green tea"`, `"black tea"`, and `"oolong tea"`. 
+   Access the first element of the array and store it in a variable named `firstTea`.
+*/
+let teaFlavors=["green tea", "black tea", "oolong tea"];
+let firstTea= teaFlavors[0];
+console.log(firstTea);
+
+/* 
+2. Declare an array named `cities` containing `"London"`, `"Tokyo"`, `"Paris"`, and `"New York"`. 
+   Access the third element in the array and store it in a variable named `favoriteCity`.
+*/
+
+let cities= ["London","Tokyo","Paris","New York"];
+let favoriteCity= cities[2];
+console.log(favoriteCity);
+
+/* 
+3. You have an array named `teaTypes` containing `"herbal tea"`, `"white tea"`, and `"masala chai"`. 
+   Change the second element of the array to `"jasmine tea"`.
+*/
+let teaTypes= ["herbal tea","white tea","masala chai"];
+teaTypes[1]= "jasmine tea";
+console.log(teaTypes);
+
+/* 
+4. Declare an array named `citiesVisited` containing `"Mumbai"` and `"Sydney"`. 
+   Add `"Berlin"` to the array using the `push` method.
+*/
+let citiesVisited= ["Mumbai", "Sydney"];
+citiesVisited.push("Berlin");
+console.log(citiesVisited);
+
+
+/* 
+5. You have an array named `teaOrders` with `"chai"`, `"iced tea"`, `"matcha"`, and `"earl grey"`. 
+   Remove the last element of the array using the `pop` method and store it in a variable named `lastOrder`.
+*/
+let teaOrders= ["chai","iced tea","matcha","earl grey"];
+let lastOrder=teaOrders.pop();
+console.log(lastOrder);
