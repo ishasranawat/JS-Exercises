3ad5e4a2e7d94823dfdc47df610392951fb729fc
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-02-03 14:57:13 +0530

    Update types.js

--- udemy/types.js
+++ udemy/types.js
@@ -10,14 +10,37 @@ function stringToNumber(input) {
 
 
 /*Task 2: Write a function flipBoolean that takes any input and converts it to its boolean equivalent, then flips it. For example, true becomes false, 0 becomes true, etc. */
-
+function flipBoolean(input) {
+    let boolval=Boolean(input)
+    if (boolval){
+        return false;
+    }else{
+        return true;
+    }
+}
 
 
 /*
 Task 3: Write a function whatAmI that takes an input and returns a string describing its type after conversion. If it's a number, return "I'm a number!", if it's a string, return "I'm a string!" */
-
+function whatAmI(input) {
+    if (typeof input === "number"){
+        return "I'm a number!"
+    }
+    else{
+        return "I'm a string!"
+    }
+}
 
 
 
 /*
 Task 4: Write a function isItTruthy that takes an input and returns "It's truthy!" if the value is truthy in JavaScript, or "It's falsey!" if it's falsey. */
+function isItTruthy(input) {
+    if (input){
+        return "It's truthy!"
+    }
+    else{
+        return "It's falsey!"
+    }
+}
+
