4cab88a3c5a19565e0936a1d02feef9240c328c7
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-02-03 14:02:22 +0530

    Create conditionals.js

---
+++ udemy/conditionals.js
@@ -0,0 +1,58 @@
+// Check if a number is greater than other
+
+let num1=20;
+let num2=5;
+
+if (num1>num2) {
+  console.log("num1 is greater.");
+}
+else{
+  console.log("num2 is greater.");
+}
+
+
+// Check if string is equal to another string
+
+let str1= "hellooo";
+let str2="hello";
+
+
+if(str1== str2){
+  console.log("str1 and str2 are equal.");
+}
+
+else{
+  console.log("they are not equal.");
+}
+
+
+// check if the variable is number or not
+
+let data=50;
+if (typeof data === "number"){
+  console.log("The data entered is a number.");
+}
+else{
+  console.log("its not a number.")
+}
+
+// check if the boolean value is true or not
+
+let vall= false;
+if(vall){
+  console.log("The value is true.");
+}
+else{
+  console.log("The value is not true.")
+}
+
+
+// check is array is empty or not
+
+let arr1= [];
+if (arr1.length==0){
+  console.log("The array is empty.");
+}
+else{
+  console.log("The array is not empty.")
+}
